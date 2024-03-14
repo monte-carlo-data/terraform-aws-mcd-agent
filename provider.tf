8e929353ad916afe3f856c0fb7723060be24ec6d
Nick Vladiceanu <nicolae.vladiceanu@olx.com>
2024-03-14 10:55:38 +0100

    remove provider.tf

--- provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = var.region
-  default_tags {
-    tags = {
-      "mcd-agent-service-name"    = lower(local.mcd_agent_service_name)
-      "mcd-agent-deployment-type" = lower(local.mcd_agent_deployment_type)
-    }
-  }
-}
